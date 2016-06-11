e4b66e6b22cb2dac0d86d7dfc22db7d7068d012f
wanjun <liuwanjunniki@outlook.com>
2016-06-11 23:06:22 +0800

    update

--- src/scala/Second/Programming4.scala
+++ src/scala/Second/Programming4.scala
@@ -4,6 +4,11 @@ package Second
   * Created by liuwanjun on 6/11/16.
   */
 
-class Programming4 {
+object Programming4 extends App{
+  def salt()={println("salt");false}
+  def pepper()={println("pepper");true}
+  println(salt() && pepper())
+  println(pepper() && salt())
 
+  println(~1)   //~按位异或运算
 }
